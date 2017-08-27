7cd7d2a14e15f6d3a898385012ae4418342d27dd
sus304 <sus304.tanaka@gmail.com>
2017-08-28 06:09:47 +0900

    add ver.3.1

--- ForRocket.py
+++ ForRocket.py
@@ -1,46 +1,96 @@
 # -*- coding: utf-8 -*-
-import os
+import platform
 import sys
-import json
+import subprocess as sp
+import numpy as np
+import os
+import datetime
+import simplekml
+from SubScript.coordinate import *
 
-print ('6-DoF Rocket Flight Simulator')
-print ('ForRocket')
+print "Simulation Start!"
+try:
+  print "cs2data"
+  import cs2data
+  print ""
+  print "Calcutate"
+  if 'Windows' == platform.system():
+	 sp.call(['Simulation'])
+  else:
+  	sp.call(['./Simulation.exe'])
+  print ""
+  print "Simulation End!"
+except:
+  print "Error : Can't Calcutate...script exit"
 
-import rocket_dynamics as Rocket
-import plot_result as Result
+#kmlファイルの出力スイッチ。0で出力オフ、1で出力オン
+kml_frag = 1
+#出力の機体名
+name = u'H-xx'
+#射点緯度経度高度[deg,deg,m]
+Launch_LLH = np.empty(3)
+Launch_LLH[0] = 40.242865
+Launch_LLH[1] = 140.010450
+Launch_LLH[2] = 5.0
 
-# Config File Load
-argv = sys.argv
-if len(argv) < 2:
-  print ('Argument is missing')
-  print ('Usage : python ForRocket.py configFileName.json')
-  sys.exit()
-print('Config File : %s > ...Loading' % argv[1])
-ConfigFile = open(argv[1])
-json_obj = json.load(ConfigFile)
 
-# Result Directory Create
-ObjectName = json_obj.get('Name')
-ResultDir = json_obj.get('System').get('Result Directory') + '_' + ObjectName
-if os.path.exists(ResultDir):  
-  ResultDir_org = ResultDir
-  i = 1
-  while os.path.exists(ResultDir):
-    sys.stderr.write ('\r\033[K' + '%s already exists' % ResultDir)
-    sys.stderr.flush()
-    ResultDir = ResultDir_org + '_%02d' % (i)
-    i = i + 1
+if kml_frag:
+  print "kml output..."
 else:
-  print ('"%s" not found' % ResultDir)
-print ()
-print ('Making...')
-os.mkdir(ResultDir)
-print ('Result Directory : %s' % ResultDir)
+  sys.exit()
+
+#座標履歴ファイルの有無
+os.chdir("Output_Log")
+try:
+  file = open('Position_log.csv')
+except:
+  print "LogFile Not found...script exit"
+  sys.exit()
 
 
-# Rocket instance
-rocket = Rocket.Config(json_obj)
-result = Result.ResultBox()
-TimeLog, Log = Rocket.Simulation(rocket, 2.0, 45.0, ResultDir, result)
-result.debug()
+def kml_make(name,Launch_LLH):
+  Log = np.loadtxt('Position_log.csv',delimiter=",",skiprows=1)
+  array = Log[:,1]
+  array_len = len(array)
+  print ":"
+  Position_ENU = np.zeros((array_len,3))
+  Position_ENU[:,0] = np.array(Log[:,0])
+  Position_ENU[:,1] = np.array(Log[:,1])
+  Position_ENU[:,2] = np.array(Log[:,2])
+  
+  Position_ecef = np.zeros((array_len,3))
+  Position_LLH = np.zeros((array_len,3))
+  print ":"
+  for i in range(array_len):
+    Position_ecef[i,:] = ENU2ECEF(Position_ENU[i,:],Launch_LLH)
+    Position_LLH[i,:] = ECEF2LLH(Position_ecef[i,:])
+  print ":"
+  
+  header = 'Latitude,Longitude,Height'
+  np.savetxt("Result Log 1.csv",Position_LLH,fmt = '%.5f',delimiter = ',',header = header)
+  
+  kml = simplekml.Kml(open=1)
+  Log_LLH = []
+  for i in range(array_len):
+    if 0 == i % 10000:
+      Log_LLH.append((Position_LLH[i,1],Position_LLH[i,0],Position_LLH[i,2]))
+  print ":"
+  line = kml.newlinestring(name = name)
+  line.style.linestyle.width = 5
+  line.style.linestyle.color = simplekml.Color.red
+  line.extrude = 1
+  line.altitudemode = simplekml.AltitudeMode.absolute
+  line.coords = Log_LLH
+  line.style.linestyle.colormode = simplekml.ColorMode.random
+  kml.save(name + ".kml")
 
+
+
+try:
+  kml_make(name,Launch_LLH)
+  os.chdir("../")
+except:
+  print "Error : Can't make kml file...script exit"
+  os.chdir("../")
+
+print "Script Complete!"
--- README.md
+++
@@ -1,9 +0,0 @@
-# ForRocket
-6DoF Rocket Flight Simulator
-
-ForRocket calculate 3-dimention rocket trajectory.
-
-
-
-#### License
-ForRocket is released under the MIT License.
